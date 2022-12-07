aa797323db2eaea0ba1eec2474b1b755cbaf5506
muhammed <malhilfe5@gmail.com>
2022-12-07 05:27:32 +0300

    fixing

--- tesseract-ocr.sh
+++ tesseract-ocr.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 # vim: set tabstop=8:softtabstop=8:shiftwidth=8:noexpandtab
-#
 
-if [ "$1" = "ara" ];then
-	lang="$1"
-else
+if [ ! -f "$1" ];then
 	lang="eng"
+else
+	lang="$1"
 fi
 
-tmpdir=$(mktemp)
-tmpimage="image.png"
-tmptext="t_text"
+tmpdir=$(mktemp -d)
+tmpimage="$tmpdir/image.png"
+tmptext="$tmpdir/t_text"
 maim --color=0.6,0.125,0.23 -b 4 -u -s "$tmpimage"
 convert -bordercolor black  -border 20 "$tmpimage" "$tmpimage"
 tesseract-ocr "$tmpimage" "$tmptext"  -l "$lang"
-xclip -selection clipboard -in "$tmptext.txt" 
+xclip -selection clipboard -in "$tmptext.txt"
+quote="$(head $tmptext.txt)"
+herbe "tesseract-ocr.sh: copy selected text in $lang""$quote""......."
 rm -R "$tmpdir" 
