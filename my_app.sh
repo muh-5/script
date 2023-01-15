9aa06229ecb2b357152b800e3b1f00908f092744
muhammed <malhilfe5@gmail.com>
2023-01-15 22:07:39 +0300

    fixing

--- my_app.sh
+++ my_app.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 list_dir="$HOME/.local/script/my_app.list"
-app_select=$(cat $list_dir |  awk -F":" '{print $1}' | sed '/^$/d' | dmenu -l 10 -i -p "APP:")
+app_select=$(awk -F":" '{print $1}' < "$list_dir" | sed '/^$/d' | dmenu -l 10 -i -p "APP:")
 app_run=$(grep  "$app_select" "$list_dir" | awk -F":" '{ print $2}')
 $app_run
