d4ad8c0ce19c12f1e361c91465b3d1d0a3e73156
muhammed <malhilfe5@gmail.com>
2023-01-24 01:44:05 +0300

    more line

--- my_app.sh
+++ my_app.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 list_dir="$HOME/.local/script/my_app.list"
-app_select=$(awk -F":" '{print $1}' < "$list_dir" | sed '/^$/d' | dmenu -l 10 -i -p "APP:")
+app_select=$(awk -F":" '{print $1}' < "$list_dir" | sed '/^$/d' | dmenu -l 15 -i -p "APP:")
 app_run=$(grep  "$app_select" "$list_dir" | awk -F":" '{ print $2}')
 $app_run
