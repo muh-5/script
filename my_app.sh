4f58ac5c5be1c741a81d9cc51dcca84a6d712371
muhammed <malhilfe5@gmail.com>
2023-04-01 22:40:54 +0300

    chatGBT was wrong

--- my_app.sh
+++ my_app.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 
-# Set a variable for the list file location
-LIST_DIR="$HOME/.local/script/my_app.list"
-
-# Use awk to extract the app names and commands from the list file
-APP_INFO=$(awk -F":" '{print $1 " " $2}' "$LIST_DIR")
-
-# Use dmenu to prompt the user to select an app from the list
-SELECTED_APP=$(echo "$APP_INFO" | awk '{print $1}' | dmenu)
-
-# Get the selected app's command from the app info
-APP_COMMAND=$(echo "$APP_INFO" | awk -v app="$SELECTED_APP" '$1 == app {print $2}')
-
-# Run the selected app's command
-exec "$APP_COMMAND"
+list_dir="$HOME/.local/script/my_app.list"
+app_select=$(awk -F":" '{print $1}' < "$list_dir" | sed '/^$/d' | dmenu -l 15 -i -p "APP:")
+app_run=$(grep  "$app_select" "$list_dir" | awk -F":" '{ print $2}')
+eval "$app_run"
