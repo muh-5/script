0b0cc6d93e47ea9e3844cb05589b899454fbc49e
muhammed <malhilfe5@gmail.com>
2023-04-01 22:52:18 +0300

    fix

--- my_app.sh
+++ my_app.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-list_dir="$HOME/.local/script/my_app.list"
-app_select=$(awk -F":" '{print $1}' < "$list_dir" | sed '/^$/d' | dmenu -l 15 -i -p "APP:")
-app_run=$(grep  "$app_select" "$list_dir" | awk -F":" '{ print $2}')
-eval "$app_run"
+# Set a variable for the list file location
+LIST_DIR="$HOME/.local/script/my_app.list"
+
+# Use awk to extract the app names from the list file and remove any blank lines
+APP_NAMES=$(awk -F":" '!/^$/ {print $1}' "$LIST_DIR")
+
+# Use dmenu to prompt the user to select an app from the list
+SELECTED_APP=$(echo "$APP_NAMES" | dmenu -l 15 -i -p "APP:")
+
+# Use grep to find the selected app's command in the list file
+APP_COMMAND=$(grep "^${SELECTED_APP}:" "$LIST_DIR" | awk -F":" '{print $2}')
+
+# Run the selected app's command
+eval "$APP_COMMAND"
